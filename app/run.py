2164cbe7e86b307f442161c809077d3051153474
Maycol <maycolteles@hotmail.com>
2023-04-11 20:47:28 -0300

    feat: adding the run.py file to start the application.

---
+++ app/run.py
@@ -0,0 +1,16 @@
+"""
+Module containing the run() function
+"""
+
+from src.main import main
+
+
+def run() -> None:
+    """
+    Function to start the application.
+    """
+    main()
+
+
+if __name__ == "__main__":
+    run()
