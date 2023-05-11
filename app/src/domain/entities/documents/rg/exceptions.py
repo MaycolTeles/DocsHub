86d6e0ffa3e99c650062d26a5cd841572664d044
Maycol <maycolteles@hotmail.com>
2023-05-10 21:39:52 -0300

    feat: implementing the RG class.

---
+++ app/src/domain/entities/documents/rg/exceptions.py
@@ -0,0 +1,10 @@
+"""
+Module containing all RG exceptions.
+"""
+
+
+class InvalidRGException(Exception):
+    """
+    Class to represent an InvalidRGException.
+    This exceptions should be raised when a RG is invalid.
+    """
